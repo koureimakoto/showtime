6a87eb0466bf7ebb869741489362fb43c0705072
Talles Fagundes <makoto.git@gmail.com>
2022-09-19 19:36:52 -0300

    Completed the Guessing Game Code in Rust Tutorial

--- Rust/BasisOfLanguage/guessing_game/src/main.rs
+++ Rust/BasisOfLanguage/guessing_game/src/main.rs
@@ -4,34 +4,41 @@ use rand::Rng;
 
 fn
 main() {
-    let mut guess : String = String::new();
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Guess the number");
 
-    println!("Please input your guess;");
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-
-    let 
-    guess: u32 = (
-        guess
+    loop {
+        let mut guess : String = String::new();
+        println!("Please input your guess;");
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        
+        let 
+        guess: u32 = (
+            guess
             .trim()
-            .parse()
-            .expect( "Please type a  number!" )
-    );
-
-    println!("Your Guessed: {guess}");
-
-    println!("Thre secret number is: {secret_number}");
-    match guess.cmp( &secret_number ) {
-        Ordering::Less    => println!( "Too small!" ),
-        Ordering::Greater => println!( "Too big!"   ),
-        Ordering::Equal   => println!( "YOU WIN!"   ),
+            .parse() {//.expect( "Please type a  number!" ) match?
+                Ok(num) => num,
+                Err(_)  => continue,
+            }
+        );
+        
+        println!("Your Guessed: {guess}");
+        match guess.cmp( &secret_number ) {
+            Ordering::Less    => println!( "Too small!" ),
+            Ordering::Greater => println!( "Too big!"   ),
+            Ordering::Equal   => {
+                println!( "YOU WIN!" ); 
+                break;
+            }
+        }
     }
-    
+    println!("Thre secret number is: {secret_number}");
+
 }
 
 /*
