e4135811792610fb500dab60f4da21d3bc358236
Talles Fagundes <makoto.git@gmail.com>
2022-09-20 21:54:55 -0300

    Testing the basic Rust funcionalities

--- Rust/BasisOfLanguage/common_programing_concepts/src/main.rs
+++ Rust/BasisOfLanguage/common_programing_concepts/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 
 fn
 main() {
@@ -8,16 +9,64 @@ main() {
     let x = 6;
     println!( "The value of x is: {x}" );
 
-    let x: String;
+    let mut x: String;
 
     x = "Hello".to_string();
 
     println!( "The new value of x is: {}", x );
 
-    const Y: &str = "AdeusMut";
-    println!( "The new value of x is: {}",  &&&Y );
+
+    // --- 
+    let mut crash: String = String::new();
+
+    io::stdin()
+        .read_line( &mut crash )
+        .expect( "Failed to read this input" );
+
+    if  crash.eq( &String::from(&crash) ) {
+        x = 6.to_string();//"Xii".to_string();
+        println!("entrou");   
+    }
+
+    println!( "Result: {x}" );
+
+    // ---
+
+    println!( "Float Test" );
+
+    let quotient = 50 / 60 ;
+    println!( "Quotient without floating typing representation: {quotient}" );
+
+
+    let quotient = 50.0 / 60.0 ;
+    println!( "Quotient without floating typing representation: {quotient}" );
+
+
+    let quotient: f32 = 50.0 / 60.0 ;
+    println!( "Quotient without floating typing representation: {quotient}" );
+
+    println!( "Remainder: {}", quotient / 4.0 );
+
+    // --- Tuples
+    println!( "Tuples Test" );
+    
+    let tup: (u8, f64, &str) = ( 1, 5.5, "Hello" );
+
+    let (x, y, z) = tup; 
+
+    println!( "Tuples values: {} {} {} ", x, y, z );
+    println!( "Tuples values of \n tup.1: {}\n tup.2: {}", tup.1, tup.2 );
+
+    // --- Array
+    println!( "Array Test" );
+
+    let arr: [char; 3] = ['o','i','\n'];
+ 
+    println!( 
+        "Array value of the \n first position: {} \n second position: {}",
+        arr[0],
+        arr[1]
+    );
 
 }
-
-
 
