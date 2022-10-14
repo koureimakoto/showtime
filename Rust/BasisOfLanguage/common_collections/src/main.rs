57b192b5427f053c71c8cb1c8607dfec5ed0cf61
Talles Fagundes <makoto.git@gmail.com>
2022-10-14 18:01:48 -0300

    Testing insec and secutiry acess in enum array positon

--- Rust/BasisOfLanguage/common_collections/src/main.rs
+++ Rust/BasisOfLanguage/common_collections/src/main.rs
@@ -1,3 +1,10 @@
+
+enum Multype {
+    Int(i32),
+    Float(f64),
+    Utf8String(String)
+}
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
 
@@ -35,5 +42,31 @@ fn main() {
         *i += 50;
         println!( "{}", i );
     }
+
+    // ---
+    
+    let row = vec![
+        Multype::Int(3),
+        Multype::Float(13.13),
+        Multype::Utf8String(String::from("Color"))
+    ];
+
+    let opt = &row[0];
+    match opt {
+        Multype::Int(el) => println!("Element {}", el),
+        _  => println!("None")
+    }
+
+    let opt = row.get(3);
+    match opt {
+        Some(el)=> {
+            match el {
+                Multype::Float(intern_el) => println!("Element {}", intern_el),
+                _ => println!("Não foi hoje")
+            }
+        },
+        _  => println!("Qualquer coisa")
+    }
+
 
 }
