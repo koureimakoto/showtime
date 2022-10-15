af02896e7a0b2b9275c29a94e9bb83ef9cf3469e
Talles Fagundes <makoto.git@gmail.com>
2022-10-15 15:59:03 -0300

    Error Handling tests with Rust

---
+++ Rust/BasisOfLanguage/error_handling/src/main.rs
@@ -0,0 +1,87 @@
+use std::fs::{File, self};
+use std::io::{self, Read, ErrorKind};
+
+fn read_username_from_file(path: &str) -> Result<String, io::Error> {
+    let uname_result = File::open(path);
+
+    let mut
+    uname_file: File = match uname_result {
+        Ok(file) => file,
+        Err(e)  => return Err(e)
+    };
+
+    let mut uname: String = String::new();
+
+    match uname_file.read_to_string(&mut uname) {
+        Ok(_)  => Ok(uname),
+        Err(e) => Err(e)
+    }
+}
+
+fn read_uname_from_file(path: &str) -> Result<String, io::Error> {
+    let mut uname_file: File   = File::open(path)?;
+    let mut uname     : String = String::new();
+    uname_file.read_to_string(&mut uname)?;
+    Ok(uname)
+}
+
+// Esta ultima implementação aqui é muito legal
+fn read_uname_from_file_short(path: &str) -> Result<String, io::Error> {
+    let mut uname: String = String::new();
+    File::open(path)?.read_to_string(&mut uname)?;
+    Ok(uname)
+}
+
+fn read_uname_from_file_caralho_que_enxuto(path: &str) -> Result<String, io::Error> {
+    fs::read_to_string(path)
+}
+
+fn main() {
+    /*
+     * Recoverable Error with Result<T, E>
+     */
+    
+    // let file_result = File::open("bonitinho.txt");
+
+    // let file = match file_result {
+    //     Ok(fd)  => fd,
+    //     Err(e) => panic!("Problema ao abrir o arquivo:")
+    // };
+
+    let path = "bonitinho.txt";
+    let _file = File::open(path).unwrap_or_else(
+        |error| {
+            if error.kind() == ErrorKind::NotFound {
+                File::create(path).unwrap_or_else(
+                    |error| {
+                        panic!("Problemas na criação do arquivo {:?}", error);
+                    }
+                )
+            } else {
+                panic!("Problema na abertura do arquivo {:?}", error);
+            }
+        }
+    );
+
+    /*
+     * Shortcuts for Panic on Error: unwrap and excpet 
+     */
+
+    // let path = "bonitinho2.txt";
+    // let file2 = File::open(path).unwrap();
+
+    /*
+     * Propagating Error
+     */
+
+    println!("UserName: {:?}", read_username_from_file(path)   );
+    println!("UserName: {:?}", read_uname_from_file(path)      );
+    println!("UserName: {:?}", read_uname_from_file_short(path));
+    println!("UserName: {:?}", read_uname_from_file_caralho_que_enxuto(path)    );
+    println!("UserName: {:?}", read_uname_from_file_caralho_que_enxuto("oi.txt"));
+
+    /*
+     * Where the ? Operator Can Be Used 
+     */
+
+}
