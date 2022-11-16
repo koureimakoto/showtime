4a53f6ba0a1e7e7b9d25b723b82f5a34e9a8d821
Talles Fagundes <makoto.git@gmail.com>
2022-11-16 09:23:02 -0300

    Module 15 of the Rust Lang Tutorial - Tying Deref Trait and and just out of curiosity I tested unwrap the Range implementation under the (x..y) operators... really cool

--- Rust/BasisOfLanguage/smart_pointers/src/main.rs
+++ Rust/BasisOfLanguage/smart_pointers/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, RangeFrom, RangeTo ,Range};
+
 enum
 List<T>{ // Como uma estrutura tem o tamanho indefinido
     Cons(i32, Box<T>),
@@ -14,6 +16,30 @@ Msg { // Exemplo de como um extrutura tem um tamanho definido
    ChangeColor(i32, i32, i32) 
 }
 
+struct
+MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn
+    deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> MyBox<T> {
+    fn
+    new(element: T) -> MyBox<T> {
+        MyBox(element)
+    }
+}
+
+fn
+print_hello(name: &str) {
+    println!("Fuck you, {}", name)
+}
+
 fn main() {
     let b = Box::new(5); //Deref
     println!("Teste de acess: {}", b);
@@ -23,4 +49,24 @@ fn main() {
             List::Cons(3, Box::new(0))
         ))
     ));
+
+
+    let sttc = 5;
+    let deref = &sttc;
+    let deref2 = MyBox::new(*deref);
+
+    assert_eq!(5, *(deref2.deref()));
+    // assert_eq!(*deref2, *deref);
+    // assert_eq!(&deref2, &deref)
+
+    let name = MyBox::new(String::from("Rust"));
+    print_hello("Carlo");
+    print_hello(&name);
+    print_hello(name.deref());
+    print_hello(&(*name)[2..]);
+    // Testing RUST from the real implementation
+    print_hello(&(*name)[Range{ start:1, end: 3 }]); // Range is equal to(x..y)
+    print_hello(&(*name)[RangeTo{ end: 2 }       ]); // RangeTo is equal to (x..)
+    print_hello(&(*name)[RangeFrom{ start: 2 }   ]); // RangeFrom is equal to (x..)
+
 }
