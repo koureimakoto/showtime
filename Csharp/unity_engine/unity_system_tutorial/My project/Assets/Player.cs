35065cab0db623c944d13c883d66f82f36a3202e
Talles Fagundes <makoto.git@gmail.com>
2022-12-08 21:42:02 -0300

    Added Jump force

--- Csharp/unity_engine/unity_system_tutorial/My project/Assets/Player.cs
+++ Csharp/unity_engine/unity_system_tutorial/My project/Assets/Player.cs
@@ -6,15 +6,19 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 0f;
+    public float jump_force;
+ 
+    private Rigidbody2D Rig; 
+ 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {   
+        Rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         Move();
+        Jump();
     }
 
 
@@ -24,5 +28,16 @@ public class Player : MonoBehaviour
         Vector3 mov        = new Vector3(x_axis_input, 0f, 0f);
 
         transform.position += mov * delta;
+    }
+
+    void Jump() {
+        bool y_axis_input = Input.GetButtonDown("Jump");
+        if(y_axis_input) {
+            Vector2 jump = new Vector2(0f, jump_force);
+            Rig.AddForce(
+                jump,
+                ForceMode2D.Impulse
+            );
+        }
     }
 }
